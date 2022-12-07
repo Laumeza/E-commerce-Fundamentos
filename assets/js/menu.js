bb919f41738f3672b68a0360d5b43022ef3000bc
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2022-12-07 15:14:43 -0500

    danos

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -10,8 +10,6 @@ addEventListener(`DOMContentLoaded`, () => {
     }
 })
 
-window.addEventListener( "scroll", function(){
-    var header = this.document.querySelector("header");
-    header.classList.toggle("abajo",window.scrollY>0);
+document.addEventListener('DOMContentLoaded', function () {
+    headerScroll()
 })
-
